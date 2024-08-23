3da6380adb225ee97dde13ab587fd90e20486d3c
Dina Tamas <53911660+dinatamas@users.noreply.github.com>
2024-08-23 16:05:55 +0200

    transfer to functions

--- utazzunk.py
+++ utazzunk.py
@@ -12,52 +12,15 @@ from tqdm import tqdm
 RYANAIR_CUTOFF = 40_000
 FLIXBUS_CUTOFF = 15_000
 
-LEAVE_FROM = datetime(2024, 9, 24)
-LEAVE_TO = datetime(2024, 9, 25)
-RETURN_FROM = datetime(2024, 9, 28)
-RETURN_TO = datetime(2024, 9, 29)
+DEPART_FROM = datetime(2024, 9, 24)
+DEPART_TO = datetime(2024, 9, 25)
+ARRIVE_FROM = datetime(2024, 9, 28)
+ARRIVE_TO = datetime(2024, 9, 29)
 
 MAX_DUR = timedelta(hours=10)
 
 # Less popular Flixbus destinations to still include
 INTERESTING_CITIES = ["Kotor"]
-
-
-class SuperRyanair(Ryanair):
-    def supersearch(self, airports):
-        best = dict()
-        for airport in airports:
-            trips = self.get_cheapest_return_flights(
-                airport,
-                LEAVE_FROM,
-                LEAVE_TO,
-                RETURN_FROM,
-                RETURN_TO,
-            )
-            for trip in trips:
-                dest = trip.outbound.destination
-                if dest not in best:
-                    best[dest] = [trip.outbound, trip.inbound]
-                else:
-                    # Calculate outbound.
-                    curr, prev = trip.outbound, best[dest][0]
-                    cheaper = min(curr, prev, key=attrgetter("price"))
-                    pricier = max(curr, prev, key=attrgetter("price"))
-                    if pricier.origin == "BUD":
-                        if pricier.price - cheaper.price > 10000:
-                            cheaper, pricier = pricier, cheaper
-                    best[dest][0] = cheaper
-
-                    # Calculate inbound
-                    curr, prev = trip.inbound, best[dest][1]
-                    cheaper = min(curr, prev, key=attrgetter("price"))
-                    pricier = max(curr, prev, key=attrgetter("price"))
-                    if pricier.destination == "BUD":
-                        if pricier.price - cheaper.price > 10000:
-                            cheaper, pricier = pricier, cheaper
-                    best[dest][1] = cheaper
-        best = dict(sorted(best.items(), key=lambda t: t[1][0].price + t[1][1].price))
-        return best
 
 
 #
@@ -94,22 +57,123 @@ class SuperFlixbus(Flixbus):
         return city_id
 
 
+def get_flixbus(
+        cutoff=FLIXBUS_CUTOFF,
+        depart_from=DEPART_FROM,
+        depart_to=DEPART_TO,
+        arrive_from=ARRIVE_FROM,
+        arrive_to=ARRIVE_TO,
+    ):
+    FLIXBUS_URL = "https://global.api.flixbus.com"
+    BUDAPEST = "40de6527-8646-11e6-9066-549f350fcb0c"
+    REACHABLE_URL = f"{FLIXBUS_URL}/cms/cities/{BUDAPEST}/reachable"
+    PARAMS = {"language": "hu", "limit": 1000, "currency": "HUF"}
+    response = requests.get(REACHABLE_URL, params=PARAMS).json()
+
+    CITIES = list()
+    COUNTRIES = dict()
+    for c in response["result"]:
+        if c["country"] == "HU":
+            continue
+        if "repülőtér" in c["name"]:
+            continue
+        # "Less interesting" destinations with many Flixbus stations.
+        if c["name"] not in INTERESTING_CITIES:
+            if c["country"] in ("HR", "SK", "RO", "PL"):
+                if c["search_volume"] < 50_000:
+                    continue
+        if "price" in c:
+            if c["price"]["HUF"]["min"] < cutoff:
+                CITIES.append(c)
+                COUNTRIES[c["country"]] = COUNTRIES.get(c["country"], []) + [c]
+
+    api = SuperFlixbus()
+
+    best = []
+    for city in tqdm(CITIES, leave=False):
+        name = city["name"]
+        if "Parndorf" in name:
+            name = "Parndorf"
+        try:
+            start = depart_from.strftime("%d-%m-%Y")
+            end = depart_to.strftime("%d-%m-%Y")
+            lbest = api.get_cheapest_for_range(start, end, BUDAPEST, city["uuid"])
+            if lbest.price < cutoff:
+                start = arrive_from.strftime("%d-%m-%Y")
+                end = arrive_to.strftime("%d-%m-%Y")
+                rbest = api.get_cheapest_for_range(start, end, BUDAPEST, city["uuid"])
+                total = int(lbest.price + rbest.price)
+                if total < cutoff * 2:
+                    ltime = lbest.departure_date_time.strftime("%m.%d. %H:%M")
+                    ldur = lbest.arrival_date_time - lbest.departure_date_time
+                    rtime = rbest.departure_date_time.strftime("%m.%d. %H:%M")
+                    rdur = rbest.arrival_date_time - rbest.departure_date_time
+                    if ldur > MAX_DUR or rdur > MAX_DUR:
+                        continue
+                    best.append(
+                        (
+                            total,
+                            name,
+                            city["country"],
+                            f"{ltime} ({str(ldur)})",
+                            f"{rtime} ({str(rdur)})",
+                        )
+                    )
+        except Exception:
+            pass
+    return sorted(best)
+
+
+def get_ryanair(
+        extended=False,
+        cutoff=RYANAIR_CUTOFF,
+        depart_from=DEPART_FROM,
+        depart_to=DEPART_TO,
+        arrive_from=ARRIVE_FROM,
+        arrive_to=ARRIVE_TO,
+    ):
+    api = Ryanair("HUF")
+    airports = ["BUD", "BTS", "VIE"] if extended else ["BUD"]
+    best = dict()
+    for airport in airports:
+        trips = api.get_cheapest_return_flights(
+            airport,
+            depart_from,
+            depart_to,
+            arrive_from,
+            arrive_to,
+        )
+        for trip in trips:
+            dest = trip.outbound.destination
+            if dest not in best:
+                best[dest] = [trip.outbound, trip.inbound]
+            else:
+                # Calculate outbound.
+                curr, prev = trip.outbound, best[dest][0]
+                cheaper = min(curr, prev, key=attrgetter("price"))
+                pricier = max(curr, prev, key=attrgetter("price"))
+                if pricier.origin == "BUD":
+                    if pricier.price - cheaper.price > 10000:
+                        cheaper, pricier = pricier, cheaper
+                best[dest][0] = cheaper
+
+                # Calculate inbound
+                curr, prev = trip.inbound, best[dest][1]
+                cheaper = min(curr, prev, key=attrgetter("price"))
+                pricier = max(curr, prev, key=attrgetter("price"))
+                if pricier.destination == "BUD":
+                    if pricier.price - cheaper.price > 10000:
+                        cheaper, pricier = pricier, cheaper
+                best[dest][1] = cheaper
+    best = dict(sorted(best.items(), key=lambda t: t[1][0].price + t[1][1].price))
+    best = [t for t in best.values() if t[0].price + t[1].price < cutoff]
+    return best
+
+
 def main():
     args = parse_args()
 
-    api = SuperRyanair("HUF")
-
-    airports = ("BUD", "BTS", "VIE") if args.A else ("BUD",)
-
-    #
-    # Note: when there is no overlap between leave and return,
-    # the best return flights are also the best individual
-    # inbound and outbound flights separately.
-    #
-
-    best = api.supersearch(airports)
-    best = [t for t in best.values() if t[0].price + t[1].price < RYANAIR_CUTOFF]
-
+    best = get_ryanair(extended=args.A)
     print()
     print("===== RYANAIR =====")
     print()
@@ -131,68 +195,9 @@ def main():
         print("No destinations are cheap enough")
     print()
 
-    FLIXBUS_URL = "https://global.api.flixbus.com"
-    BUDAPEST = "40de6527-8646-11e6-9066-549f350fcb0c"
-    REACHABLE_URL = f"{FLIXBUS_URL}/cms/cities/{BUDAPEST}/reachable"
-    params = {"language": "hu", "limit": 1000, "currency": "HUF"}
-    response = requests.get(REACHABLE_URL, params=params).json()
-
-    CITIES = list()
-    COUNTRIES = dict()
-    for c in response["result"]:
-        if c["country"] == "HU":
-            continue
-        if "repülőtér" in c["name"]:
-            continue
-        # "Less interesting" destinations with many Flixbus stations.
-        if c["name"] not in INTERESTING_CITIES:
-            if c["country"] in ("HR", "SK", "RO", "PL"):
-                if c["search_volume"] < 50_000:
-                    continue
-        if "price" in c:
-            if c["price"]["HUF"]["min"] < FLIXBUS_CUTOFF:
-                CITIES.append(c)
-                COUNTRIES[c["country"]] = COUNTRIES.get(c["country"], []) + [c]
-
-    api = SuperFlixbus()
-
+    best = get_flixbus()
     print("===== FLIXBUS =====")
     print()
-
-    FLIXBUS_BEST = []
-    for city in tqdm(CITIES, leave=False):
-        name = city["name"]
-        if "Parndorf" in name:
-            name = "Parndorf"
-        try:
-            start = LEAVE_FROM.strftime("%d-%m-%Y")
-            end = LEAVE_TO.strftime("%d-%m-%Y")
-            lbest = api.get_cheapest_for_range(start, end, BUDAPEST, city["uuid"])
-            if lbest.price < FLIXBUS_CUTOFF:
-                start = RETURN_FROM.strftime("%d-%m-%Y")
-                end = RETURN_TO.strftime("%d-%m-%Y")
-                rbest = api.get_cheapest_for_range(start, end, BUDAPEST, city["uuid"])
-                total = int(lbest.price + rbest.price)
-                if total < FLIXBUS_CUTOFF * 2:
-                    ltime = lbest.departure_date_time.strftime("%m.%d. %H:%M")
-                    ldur = lbest.arrival_date_time - lbest.departure_date_time
-                    rtime = rbest.departure_date_time.strftime("%m.%d. %H:%M")
-                    rdur = rbest.arrival_date_time - rbest.departure_date_time
-                    if ldur > MAX_DUR or rdur > MAX_DUR:
-                        continue
-                    FLIXBUS_BEST.append(
-                        (
-                            total,
-                            name,
-                            city["country"],
-                            f"{ltime} ({str(ldur)})",
-                            f"{rtime} ({str(rdur)})",
-                        )
-                    )
-        except Exception:
-            pass
-    FLIXBUS_BEST.sort()
-
     tprint(
         ["Price", "Destination", "Leave", "Return"],
         *[
@@ -201,21 +206,21 @@ def main():
                 f"{name} ({country})",
                 dep,
                 ret,
-            ] for price, name, country, dep, ret in FLIXBUS_BEST
+            ] for price, name, country, dep, ret in best
         ]
     )
-    if args.s:
-        print("Stats:")
-        for name, cities in sorted(COUNTRIES.items()):
-            print(f"  - {name}: {len(cities)}")
-        print()
-    if args.c:
-        for name, cities in sorted(COUNTRIES.items()):
-            print()
-            print(f"### {name}: ###")
-            for c in cities:
-                print(f"   {c['name']} ({c['slug']}) - {c['price']['HUF']['min']}")
-        print()
+    #if args.s:
+    #    print("Stats:")
+    #    for name, cities in sorted(COUNTRIES.items()):
+    #        print(f"  - {name}: {len(cities)}")
+    #    print()
+    #if args.c:
+    #    for name, cities in sorted(COUNTRIES.items()):
+    #        print()
+    #        print(f"### {name}: ###")
+    #        for c in cities:
+    #            print(f"   {c['name']} ({c['slug']}) - {c['price']['HUF']['min']}")
+    #    print()
     print()
 
 
@@ -239,8 +244,8 @@ def tprint(*rows):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-A", action="store_true", help="include BTS+VIE airports too")
-    parser.add_argument("-c", action="store_true", help="print Flixbus cities")
-    parser.add_argument("-s", action="store_true", help="print Flixbus stats")
+    #parser.add_argument("-c", action="store_true", help="print Flixbus cities")
+    #parser.add_argument("-s", action="store_true", help="print Flixbus stats")
     return parser.parse_args()
 
 
